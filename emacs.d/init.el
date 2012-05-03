881131f4ecf4fddd17482c4397ad65d78ee09865
Kyle Partridge <kyle.partridge@sendgrid.com>
2012-05-03 10:16:27 -0700

    added the el-get for installing packages

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,6 +3,7 @@
 (push "/usr/local/bin" exec-path)
 (add-to-list 'load-path "~/.emacs.d")
 
+(set-frame-font "Meslo LG M DZ-14")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
 (load-theme 'solarized-light t)
 
@@ -23,7 +24,11 @@
 (set-fringe-style -1)
 (tooltip-mode -1)
 
-(set-frame-font "Meslo LG M DZ-14")
+(require 'package)
+(setq package-archives (cons '("tromey" . "http://tromey.com/elpa/") package-archives))
+(package-initialize)
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(require 'el-get)
 
 (defun set-newline-and-indent ()
   (local-set-key (kbd "RET") 'reindent-then-newline-and-indent))
